515211d5ccf83839017581b03161c5ee4f45e3eb
Izuna <izuna.seikatsu@ccbluex.net>
2025-05-16 00:30:08 +0200

    fix copyright header

--- src/main/kotlin/net/ccbluex/liquidbounce/features/misc/proxy/ProxyManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/misc/proxy/ProxyManager.kt
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with LiquidBounce If not, see <https://www.gnu.org/licenses/>.
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
  */
 package net.ccbluex.liquidbounce.features.misc.proxy
 
